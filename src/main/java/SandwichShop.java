5b8e3fdb5db551a2460756dad9be2a1b854443a8
CodeHart-art <jobhart11211@gmail.com>
2026-04-10 14:06:19 -0400

    Hard coded solution

--- src/main/java/SandwichShop.java
+++ src/main/java/SandwichShop.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SandwichShop {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        double pickedprice = 5.45;
+        double largePrice = 8.95;
+        int studentAge = 17;
+
+        if (studentAge <= 17){
+            pickedprice *= .90;
+
+            System.out.printf("your total is $%.2f",pickedprice);
+
+        } else if (studentAge >=65) {
+            pickedprice *= .80;
+
+            System.out.printf("your total is $%.2f",pickedprice);
+        }
+        else {
+            System.out.println("your total is %" + pickedprice);
+        }
+        ;
+    }
 }
